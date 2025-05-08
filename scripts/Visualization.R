0e6efd204fdd8e7d9c3a22ba3a5bf5626a556ae6
Diva-h <anihovhannisyan303@gmail.com>
2025-05-08 15:40:20 +0200

    new functions from dypl

--- scripts/Visualization.R
+++ scripts/Visualization.R
@@ -11,3 +11,30 @@ qplot(mpg$hwy)
 qplot(mpg$cty)
 
 
+# using ggplot package for creating grapphics
+
+#--> Scatter plot
+ggplot(data=mpg) +
+  geom_point(mapping = aes(x=displ, y= hwy))
+
+#Continues line
+ggplot(data=mpg) +
+  geom_line(mapping = aes(x=displ, y= hwy))
+
+# Confidence intervals
+ggplot(data=mpg) +
+  geom_errorbar(mapping = aes(x=displ, y= hwy))
+
+# add text
+ggplot(data=mpg) +
+  geom_text(mapping = aes(x=displ, y= hwy))
+
+#Smooth plot
+ggplot(data=mpg)+
+  geom_smooth(mapping=aes(x=displ,y=hwy))
+
+
+
+#--> Scatter plot with colors
+ggplot(data=mpg) +
+  geom_point(mapping = aes(x=displ, y= hwy),color="red")
